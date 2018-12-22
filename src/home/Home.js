9556d6a8eaba527709389c8b859c344880a769f6
vijayst <vijayst@gmail.com>
2018-12-22 21:26:58 +0530

    page navigation

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Home() {
     return (
-        <div>Home component</div>
+        <div>
+            <h1>Home component</h1>
+            <p>Both the below navigation should work!</p>
+            <a href="/users">Users via server render</a><br />
+            <Link to="/users">Users via react router link</Link>
+        </div>
     )
 }
--- src/users/Users.js
+++ src/users/Users.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Users() {
     return (
-        <div>Users component</div>
-    )
+        <div>
+            <h1>Users component</h1>
+            <Link to="/">Back to home page</Link>
+            <h2>List of users</h2>
+        </div>
+    );
 }

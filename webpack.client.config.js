91ea40518b4f4b7fbe6ff71a91c4fddc8ab279d0
vijayst <vijayst@gmail.com>
2018-12-22 16:49:54 +0530

    webpack - client bundle

---
+++ webpack.client.config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: ['webpack/hot/only-dev-server', './src/client/index.js'],
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: 'bundle.js',
+        publicPath: '/'
+    },
+    module: {
+        rules: [
+            {
+                test: /.js$/,
+                loader: 'babel-loader',
+                include: path.resolve(__dirname, 'src'),
+                exclude: /node_modules/,
+                options: {
+                    presets: [['env', { modules: false }], 'react'],
+                    plugins: [
+                        'react-hot-loader/babel',
+                        ['transform-object-rest-spread', { useBuiltIns: true }],
+                        'transform-class-properties'
+                    ]
+                }
+            },
+            {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract(['css-loader', 'sass-loader'])
+            },
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract(['css-loader'])
+            },
+            {
+                test: /\.(jpg|png|svg|gif|pdf)$/,
+                loader: 'file-loader',
+                options: {
+                    name: '[path][name].[ext]'
+                }
+            }
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin('styles.css'),
+        new webpack.HotModuleReplacementPlugin()
+    ]
+};
